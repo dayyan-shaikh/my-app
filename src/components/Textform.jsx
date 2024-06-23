1ac0dbba8f44a4957e19802e46db6e48919b28a3
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2024-06-23 16:59:02 +0530

    commit

--- src/components/Textform.jsx
+++ src/components/Textform.jsx
@@ -1,44 +1,59 @@
-import React, {useState} from 'react'
-
+import React, { useState } from "react";
 
 export default function Textform(props) {
-    const handleUpClick=() =>{
-        let newText=text.toUpperCase();
-        setText(newText )
-    }
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
 
-    const handleloClick=() =>{
-        let newText=text.toLowerCase();
-        setText(newText )
-    }
-    const handleClearClick=() =>{
-        let newText='';
-        setText(newText);
-    }
+  const handleloClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+  const handleClearClick = () => {
+    let newText = "";
+    setText(newText);
+  };
 
-    const handleOnChange=(event) =>{
-        setText(event.target.value);
-    }
-    
-    const [text,setText] = useState("");
-    
-    return (
-        <>
-        <div className='container'>
-                <h1>{props.heading}</h1>
-                <div className="mb-3">
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="my-box" rows="3"></textarea>
-                </div>
-                    <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to uppercase</button>
-                    <button className="btn btn-primary mx-1 " onClick={handleloClick}>Convert to lowercase</button>
-                    <button className="btn btn-primary mx-1 " onClick={handleClearClick}>Clear text</button>
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const [text, setText] = useState("");
+
+  return (
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="my-box"
+            rows="3"
+          ></textarea>
         </div>
-        <div className='container my-3'>
-                <h1>Your Text Summary</h1>
-                <p>{text.length > 0 ? text.split(" ").length: 0} words and {text.length} characters</p>
-                <p>{0.008*text.length > 0 ? text.split(" ").length: 0}Minutes read</p>
-                
-        </div>
-        </>
-  )
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+          Convert to uppercase
+        </button>
+        <button className="btn btn-primary mx-1 " onClick={handleloClick}>
+          Convert to lowercase
+        </button>
+        <button className="btn btn-primary mx-1 " onClick={handleClearClick}>
+          Clear text
+        </button>
+      </div>
+      <div className="container my-3">
+        <h1>Your Text Summary</h1>
+        <p>
+          {text.length > 0 ? text.split(" ").length : 0} words and {text.length}{" "}
+          characters
+        </p>
+        <p>
+          {0.008 * text.length > 0 ? text.split(" ").length : 0}Minutes read
+        </p>
+      </div>
+    </>
+  );
 }
